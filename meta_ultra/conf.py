7cd400ab86beef7d9c7a7ff582f93cec65e477ca
Danko David <dcd3001@pascal.med.cornell.edu>
2017-01-16 16:47:37 -0500

    added wizard features to the conf builder

--- meta_ultra/conf.py
+++ meta_ultra/conf.py
@@ -1,39 +1,123 @@
 import meta_ultra.config as config
+from meta_ultra.refs import get_references
 from yaml import dump
+import sys
+
+class RefChoice:
+	def __init__(self, name, options):
+		self.name = name
+		self.options = options
+
+	def __str__(self):
+		sys.stderr.write('Please select an option for {}:\n'.format(self.name))
+		for i, el in enumerate(self.options):
+			sys.stderr.write('\t[{}] {}\n'.format(i, el.name))
+		choice = input('Please enter the index of your choice [0]: ')
+		try:
+			choice = int(choice)
+		except ValueError:
+			choice = 0
+		
+		return self.options[choice].path
+
+class UserInput:
+	def __init__(self, prompt, default):
+		self.prompt = prompt
+		self.default = default
+
+	def __str__(self):
+		inp = input(self.prompt + '[{}]: '.format(self.default))
+		if not inp:
+			inp = self.default
+		return inp
 
 
 
-default_conf = {
-'TMP_DIR' : '/tmp',
-'READ_1_EXT' : '_1.fastq.gz',
-'READ_2_EXT' : '_2.fastq.gz',
+	
 
-'TOOLS_TO_RUN' : ['SHORTBRED'],
+class ConfBuilder:
+	def __init__(self):
+		self.global_fields = {}
+		self.tools = {}
 
-'SHORTBRED_ALL' : 'all-samples.shortbred.csv',
-'SHORTBRED_EXT' : '.shortbred.csv',
-'SHORTBRED_DB' : None,
-'SHORTBREAD_THREADS' : 4,
+	def add_global_field(self, key, value):
+		self.global_fields[key] = value
+	
+	def add_tool(self, tool_name):
+		self.tools[tool_name] = ToolBuilder(tool_name)
+		return self.tools[tool_name]
 
-'SAMPLES' : None
+	def to_dict(self):
+		out = resolve_dict(self.global_fields)
+		for tool_name, builder in self.tools.items():
+			inp = ''
+			while 'y' not in inp and 'n' not in inp:
+				inp = input('Include {} in this analysis? (y/n) [y]: '.format(tool_name))
+				if not inp:
+					inp = 'y'
+			if 'n' in inp:
+				continue
+			
+			out[tool_name] = builder.to_dict()
+		return( out)
 
-}
+class ToolBuilder:
+	def __init__(self,tool_name):
+		self.name = tool_name
+		self.fields = {}
+	
+	def add_field(self,key,value):
+		self.fields[key] = value
 
-def build_conf(samples,pairs=False):
-    conf = default_conf
-    if not pairs:
-        samples = {sample:[sample] for sample in samples}
-        conf['SAMPLES'] =  samples
-    
-    else:
-        samplePairs = {}
-        for sample in samples:
-            sampleid = sample.split('_')[0]
-            if sampleid not in samplePairs:
-                samplePairs[sampleid] = []
-            samplePairs[sampleid].append(sample)
-        conf['SAMPLES'] = samplePairs
-    
-    print( dump( conf))
-    
+	def to_dict(self):
+		return( resolve_dict( self.fields))
+
+			
+def build_conf(samples, pairs=False):
+	conf = ConfBuilder()
+
+	if not pairs:
+		samples = {sample:[sample] for sample in samples}
+		conf.add_global_field('SAMPLES', samples)
+	
+	else:
+		samplePairs = {}
+		for sample in samples:
+			sampleid = sample.split('_')[0]
+			if sampleid not in samplePairs:
+				samplePairs[sampleid] = []
+			samplePairs[sampleid].append(sample)
+		conf.add_global_field('SAMPLES', samplePairs)
+
+	
+	# global opts
+	conf.add_global_field('TMP_DIR', UserInput('Please select a temporary directory', '/tmp'))
+
+
+	# shortbred
+	shortbred = conf.add_tool('SHORTBRED')
+	shortbred.add_field('EXT', '.shortbred.csv')
+	shortbred.add_field('EXC', 'shortbred_quantify.py')
+	shortbred.add_field('DB', RefChoice('ShortBred DB',get_references(tool='shortbred'))),
+	shortbred.add_field('THREADS',4)
+	
+	print( dump( conf.to_dict()))
+
+def resolve_dict(el):
+	if type(el) == str:
+		return el
+	elif type(el) == list:
+		out = []
+		for subel in el:
+			out.append( resolve_dict(subel))
+		return out
+	elif type(el) == dict:
+		out = {}
+		for k, v in el.items():
+			out[resolve_dict(k)] = resolve_dict(v)
+		return out
+	else:
+		return str(el)
+		
+	
 
--- meta_ultra/refs.py
+++ meta_ultra/refs.py
@@ -8,3 +8,20 @@ def add_reference(tool,name,path):
 def list_references():
     print('Tool\tName\tPath')
     print(open(config.ref_file).read())
+
+class Reference:
+     def __init__(self, tool, name, path):
+         self.tool = tool
+         self.name = name
+         self.path = path
+
+def get_references(tool=None):
+    refs = []
+    with open(config.ref_file) as rf:
+        for line in rf:
+            mytool, name, path = line.strip().split()
+            if tool and mytool.lower() != tool.lower():
+                continue
+            ref = Reference(mytool, name, path)
+            refs.append(ref)
+    return(refs)
