ef825eef241731f8460eb3f0bdbd2e3c5af03cd1
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-11-02 11:09:22 -0400

    Add CircleCI config.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 
 version = {}
@@ -35,6 +39,20 @@ test_requirements = [
 ]
 
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version."""
+
+    description = 'Verify that the git tag matches our version.'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != 'v{0}'.format(__version__):
+            info = 'Git tag: {0} does not match the version of this app: {1}'
+            info = info.format(tag, __version__)
+            sys.exit(info)
+
+
 setup(
     name='meta_ultra',
     version=version['__version__'],
@@ -42,7 +60,7 @@ setup(
     long_description=readme + '\n\n' + history,
     author=version['__author__'],
     author_email=version['__email__'],
-    url='https://github.com/dcdanko/meta_ultra',
+    url='https://github.com/dcdanko/MetaUltra',
     packages=find_packages(exclude=['tests', 'scripts']),
     include_package_data=True,
     install_requires=requirements,
@@ -71,4 +89,7 @@ setup(
     ],
     test_suite='tests',
     tests_require=test_requirements,
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    },
 )
